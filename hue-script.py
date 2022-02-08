207d96bfd0e811f2a309ce6d5d7e816349860de5
Johan Kövamees <Johan@kova-tech.com>
2022-02-08 22:03:29 +0000

    Added that all lights turn on in full brightness

--- hue-script.py
+++ hue-script.py
@@ -7,7 +7,8 @@ import sys
 
 b = Bridge('192.168.1.219')
 b.get_api()
-lights = b.lights
+#lights = b.lights
+lights = b.get_light_objects('id')
 
 script_arg = sys.argv[1]
 
@@ -16,11 +17,15 @@ def turn_off_lights():
         
 def turn_on_lights(): 
     b.set_light([1,2,3,4],'on', True)
+    lights[1].brightness = 254
+    lights[2].brightness = 254
+    lights[3].brightness = 254
+    lights[4].brightness = 254
 
 if script_arg=='on':
     turn_on_lights()
 elif script_arg=='off':
     turn_off_lights()
-elif script_arg=="print":
-    for l in lights:
-        print(l.name)
+#elif script_arg=="print":
+#    for l in lights:
+#        print(l.name)
