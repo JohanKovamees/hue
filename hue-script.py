9a3809511e9acfddf6742cd66d003d9192a3c2ac
johan <johan@kova-tech.com>
2022-10-18 15:11:11 +0200

    Added web application, next is docker

--- hue-script.py
+++ hue-script.py
@@ -7,31 +7,24 @@ import sys
 
 b = Bridge('10.0.1.195')
 b.get_api()
-#lights = b.lights
+
 lights = b.get_light_objects('id')
 
 script_arg = sys.argv[1]
 
 def turn_off_lights():
-    b.set_light([8,9,10,11,12,13],'on', False)
+    for l in lights:
+        b.set_light([l],'on', False)
         
 def turn_on_lights(): 
-    b.set_light([8,9,10,11,12,13],'on', True)
-    lights[8].brightness = 254
-    lights[9].brightness = 254
-    lights[10].brightness = 254
-    lights[11].brightness = 254
-    lights[12].brightness = 254
-    lights[13].brightness = 254
+    for l in lights:
+        b.set_light([l],'on', True)
+        lights[l].brightness = 254
 
 def cozy():
-    b.set_light([8,9,10,11,12,13],'on', True)
-    lights[8].brightness = 120
-    lights[9].brightness = 120
-    lights[10].brightness = 120
-    lights[11].brightness = 120
-    lights[12].brightness = 120
-    lights[13].brightness = 120
+    for l in lights:
+        b.set_light([l],'on', True)
+        lights[l].brightness = 127
 
 if script_arg=='on':
     turn_on_lights()
---
+++ hue-web.py
@@ -0,0 +1,46 @@
+
+##Script to communicate with hue
+
+from os import getenv
+from phue import Bridge
+from flask import Flask, request
+import sys
+
+secret_key = getenv("FLASK_KEY")
+
+hue_ip = getenv("HUE_IP")
+b = Bridge(hue_ip)
+b.get_api()
+lights = b.get_light_objects('id')
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    @app.route('/off')
+    def turn_off_lights():
+        for l in lights:
+            b.set_light([l],'on', False)
+        return "Turning Off"
+        
+    @app.route('/on')
+    def turn_on_lights(): 
+        for l in lights:
+            b.set_light([l],'on', True)
+            lights[l].brightness = 254
+        return "Turning On"
+
+    @app.route('/cozy')
+    def cozy():
+        for l in lights:
+            b.set_light([l],'on', True)
+            lights[l].brightness = 127
+        return "Turning Cozy"
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0',debug=False,use_debugger=False, use_reloader=False)
+    
+    return app
+
+#elif script_arg=="print":
+#    for l in lights:
+#        print(l.name)
